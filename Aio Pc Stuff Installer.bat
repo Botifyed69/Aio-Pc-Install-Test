4ce3a1d5f55ba3109c967935377b94218cf52390
Botifyed69 <brendanm577@gmail.com>
2024-11-24 20:13:32 +0000

    Add files via upload

--- Aio Pc Stuff Installer.bat
+++ Aio Pc Stuff Installer.bat
@@ -10,8 +10,28 @@ if %Active%==n goto Quit
 exit
 :Run
 start "" https://download.gigabyte.com/FileList/Driver/mb_driver_612_realtekdch_6.0.9235.1.zip?v=7fe824bcf96330be2c702339349d216d
+start "" https://wolicheng.com/womic/downloads/WOMicClientSetup5_2.exe
+start "" https://aka.ms/vs/16/release/vc_redist.x86.exe
+echo Downloading Discord
 winget Install "Discord.Discord"
+cls
+echo Downloading Gpu Drivers
 winget Install "Nvidia.GeForceExperience"
+cls
+echo Downloading Cosair Aio Pump Control Software
 winget Install "Corsair.iCUE.3 3.38.88"
+cls
+echo Downloading Steam
 winget Install "Valve.Steam"
+cls
+echo Downloading Bambustudio
 winget Install "Bambulab.Bambustudio"
+cls
+echo -- All Downloads Finished, Do Not Close This Window!! --
+echo When All Downloads From Edge Are Installed Press y Then Enter To Restart
+set /p Restart=Finished? 
+if %Restart%==y goto RestartN
+
+:RestartN
+cls
+shutdown /r
